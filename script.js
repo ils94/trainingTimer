42bc3922fc7d583355f422a87c9024a79c0cf299
ILS <99141005+ils94@users.noreply.github.com>
2023-11-16 14:57:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,115 @@
+let countdownInterval;
+let totalTime1 = 0;
+let totalTime2 = 0;
+let remainingTime = 0;
+let laps = 0;
+let currentCooldown = 1;
+
+// Preloading audio files
+const countAudio = new Audio('sound/count.mp3');
+const tenSeconds = new Audio('sound/10_seconds.mp3');
+const beepAudio = new Audio('sound/beep.mp3');
+const finishAudio = new Audio('sound/finish.mp3');
+
+const startAudio = new Audio('sound/start.mp3');
+const restAudio = new Audio('sound/rest.mp3');
+
+function startCountdown() {
+  const time1 = parseInt(document.getElementById("inputTime1").value);
+  const time2 = parseInt(document.getElementById("inputTime2").value);
+  laps = parseInt(document.getElementById("inputLaps").value);
+
+  if (!isNaN(time1) && !isNaN(time2) && !isNaN(laps)) {
+    totalTime1 = time1;
+    totalTime2 = time2;
+    remainingTime = totalTime1;
+    currentCooldown = 1;
+    updateCountdownDisplay();
+
+    countdownInterval = setInterval(updateTimer, 1000);
+	
+	startAudio.play()
+	
+  } else {
+    alert("Please enter valid numbers for times and laps.");
+  }
+}
+
+function updateTimer() {
+  remainingTime--;
+  
+  if (remainingTime === 10) {
+    
+    tenSeconds.play();
+  }
+
+  if (remainingTime <= 5 && remainingTime > 0) {
+    if (!countAudio.paused) {
+      countAudio.pause();
+      countAudio.currentTime = 0;
+    }
+    countAudio.play();
+  }
+
+  if (remainingTime >= 0) {
+    updateCountdownDisplay();
+  } else {
+    switchCooldown();
+  }
+}
+
+function switchCooldown() {
+  if (currentCooldown === 1) {
+	  
+    remainingTime = totalTime2;
+    currentCooldown = 2;
+	
+	restAudio.play()
+	
+  } else if (currentCooldown === 2) {
+	  
+    laps--;
+
+    if (laps > 0) {
+      remainingTime = totalTime1;
+      currentCooldown = 1;
+	  
+	  startAudio.play()
+    } else {
+      clearInterval(countdownInterval);
+      remainingTime = 0;
+	  finishAudio.play()
+    }
+  }
+
+  updateCountdownDisplay();
+}
+
+function updateCountdownDisplay() {
+  const hours = Math.floor(remainingTime / 3600);
+  const minutes = Math.floor((remainingTime % 3600) / 60);
+  const seconds = remainingTime % 60;
+
+  const formattedTime = `${hours}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+  document.querySelector('.countdown').textContent = formattedTime;
+
+  if (remainingTime === 0 && beepAudio.paused) {
+    beepAudio.play();
+  }
+}
+
+function stopCountdown() {
+  clearInterval(countdownInterval);
+  totalTime1 = 0;
+  totalTime2 = 0;
+  remainingTime = 0;
+  laps = 0;
+  currentCooldown = 1;
+  document.querySelector('.countdown').textContent = '0:00:00';
+}
+
+// Function to stop playing audio
+function stopAudio(audioElement) {
+  audioElement.pause();
+  audioElement.currentTime = 0;
+}
